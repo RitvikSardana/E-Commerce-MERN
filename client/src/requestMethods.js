bbac3e3ea847dc46003ac356f0015ba686930ab0
RitvikSardana <65544983+RitvikSardana@users.noreply.github.com>
2022-09-08 12:41:08 +0530

    Changed Backend Link

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:1337/api"
+const BASE_URL = "https://e-commerce-backend.up.railway.app/api"
 // const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyZWI3YWI1YjIxZjFkNDZkNGU1MjdiNiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1OTgwMzg5NCwiZXhwIjoxNjYwMDYzMDk0fQ.s3W5TDbqKnfGpdBz7lKlqZH8OxQNnqpCkiwbNkkoyBA'
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem('persist:root')).user).currentUser?.token 
 export const publicRequest = axios.create({
